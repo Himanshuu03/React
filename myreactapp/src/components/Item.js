d296242b7bc1d490216c59d924c84cb8e1672eb9
Himanshuu03 <himanshujangra2403@gmail.com>
2023-07-17 15:42:56 +0530

    useState

--- myreactapp/src/components/Item.js
+++ myreactapp/src/components/Item.js
@@ -1,11 +1,19 @@
 import './Item.css';
+import {useState} from 'react';
 function Item(props) {
-    const itemName = props.name;
+    // const itemName = props.name;
+    //state
+    const[itemName,setItemName] = useState(props.name);
+    function clickHandler(){
+      setItemName("nirma");
+      console.log("button clicked");
+    }
     return(
-        <>
+        <div>
           <p className ={props.class}>{itemName}</p>
-          {props.children}
-        </>
+          {/* {props.children} */}
+          <button onClick={clickHandler}>Add to Cart</button>
+        </div>
     )
 }
 export default Item;
