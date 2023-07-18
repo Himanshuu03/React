dfaf33654450f6bd284275f93592a2fcc92026ff
Himanshuu03 <himanshujangra2403@gmail.com>
2023-07-18 11:00:59 +0530

    counter app

--- counter/src/App.js
+++ counter/src/App.js
@@ -1,12 +1,37 @@
 import "./App.css";
+import {useState} from 'react';
 
 function App() {
+  const [count,setCount] = useState(0);
+  function decrementHandler(){
+    setCount(count-1);
+  }
+  function incrementHandler(){
+    setCount(count+1);
+  }
+  function resetHandler(){
+    setCount(0);
+  }
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+   <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+    <div className="text-[#0398d4] font-medium text-2xl">
+      Increment & Decrement
+    </div>
+    <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+      <button onClick={decrementHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
+        -
+      </button>
+      <div className="font-bold gap-12 text-5xl">
+        {count}
+      </div>
+      <button onClick={incrementHandler} className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">
+        +
+      </button>
+    </div>
+    <button onClick={resetHandler} className="bg-[#0398d4] text-white px-5 py-5 rounded-sm text-lg">
+      Reset
+    </button>
+   </div>
   );
 }
 
